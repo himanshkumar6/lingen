eb6344d351584609637e7a899e96333db81f8830
himanshkumar6 <githubhimanshu0@gmail.com>
2026-03-02 00:07:15 +0530

    Add New Sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,6 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "/character-counter",
     "/ai-anime-name-generator",
     "/anime-name-generator",
+    "image-watermark-remover",
     "/privacy-policy",
     "/terms",
     "/disclaimer",
